c381c15c048ee09c90b4beeca4aa8cd27d7a27f7
GRASiN1 <guaravrajsingh047@gmail.com>
2025-03-04 14:18:04 -0500

    created factory for jobs and seeded database

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -2,37 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Jobs extends Model
 {
-    public static function allJobs(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Project Manager',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Web Developer',
-                'salary' => '$80,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Web Designer',
-                'salary' => '$60,000'
-            ]
-        ];
-    }
-
-    public static function findOne($id): array
-    {
-        $job = Arr::first(static::allJobs(), fn($job) => $job['id'] == $id);
-        if (!$job)
-            abort(404);
-        return $job;
-    }
+    use HasFactory;
+    protected $table = "job_listings";
+    protected $fillable = ['title', 'salary'];
 }
